de40d58233ed8424c7cb52fe5a4702107d16253d
Gor <7495500@gmail.com>
2021-04-29 19:22:39 +0300

    Add fontawesome

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,6 +5,7 @@ import "channels"
 
 import 'bootstrap'
 import '../stylesheets/application'
+import "@fortawesome/fontawesome-free/css/all.css"
 
 document.addEventListener("turbolinks:load", () => {
   $('[data-toggle="tooltip"]').tooltip();
