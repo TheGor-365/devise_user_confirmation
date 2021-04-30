747c97885577c32293759055ebd937764e2f4860
Gor <7495500@gmail.com>
2021-04-30 15:30:44 +0300

    Add sign up confirmation with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, controller: {
+    confirmations: 'confirmations'
+  }
+
   root to: 'pages#home'
+
+  resources :dashboard, only: [:index]
 
   get 'terms',    to: 'pages#terms'
   get 'contacts', to: 'pages#contacts'
